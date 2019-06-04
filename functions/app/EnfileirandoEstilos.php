bdcb10b323741a309e44d34f0e4d16fdb16fcb2b
Fábio Pimentel <fabio.pimentel@taointerativa.com.br>
2019-06-04 17:40:39 -0300

    corringindo empilhamento de estilos

--- functions/app/EnfileirandoEstilos.php
+++ functions/app/EnfileirandoEstilos.php
@@ -15,15 +15,17 @@ class EnfileirandoEstilos
     public function enfileirandoEstilo()
     {
         //Css
-        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap3/dist/css/bootstrap.min.css', array(), '');
+        wp_enqueue_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), '');
         wp_enqueue_style('font-awesome', get_template_directory_uri() . '/node_modules/font-awesome/css/font-awesome.min.css', array(), '');
         wp_enqueue_style('slick', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.css', array(), '');
         wp_enqueue_style('slick-theme', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick-theme.css', array(), '');
+        wp_enqueue_style('Montserrat', "https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900&display=swap", array(), '');
         wp_enqueue_style('template', get_template_directory_uri() . '/assets/css/template.css', array(), '');
         //Scripts
-        wp_enqueue_script('Polyfill', get_template_directory_uri() . '/node_modules/instersection-observer/intersection-observer.js', array(), null, true);
+        wp_enqueue_script('Polyfill', get_template_directory_uri() . '/node_modules/intersection-observer/intersection-observer.js', array(), null, true);
         wp_enqueue_script('jquery-nao-nativo', get_template_directory_uri() . '/node_modules/jquery/dist/jquery.js', array(), '', true);
-        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap3/dist/js/bootstrap.js', array(), '', true);
+        wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array(), '', true);
+        wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array(), '', true);
         wp_enqueue_script('vue', get_template_directory_uri() . '/node_modules/vue/dist/vue.js', array(), '', true);
         wp_enqueue_script('vue-resource', get_template_directory_uri() . '/node_modules/vue-resource/dist/vue-resource.js', array(), '', true);
         wp_enqueue_script('lozad', get_template_directory_uri() . '/node_modules/lozad/dist/lozad.js', array(), '', true);
