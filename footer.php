8bc5d5aff18a3f0d541ee057569ebd96c482f221
Fábio Pimentel <fabio.pimentel@taointerativa.com.br>
2019-09-09 16:04:42 -0300

    correção ná chamada da API recaptcha

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <?php require_once 'functions/security.php'; ?>
 </section>
 <?php wp_footer(); ?>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
 </body>
 </html>
